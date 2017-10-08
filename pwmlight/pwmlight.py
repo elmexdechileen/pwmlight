a466a30610530e44e3673a65d5e4f097eaaa3285
elmexdechileen <maxvanrooijen@gmail.com>
2017-10-08 12:37:47 +0200

    First commit

---
+++ pwmlight/pwmlight.py
@@ -0,0 +1,35 @@
+import pigpio as pg
+
+class pwmlight():
+    def __init__(self, pin, min_cycle, max_cycle, name, frequency = 2000):
+        self._pin = pin
+        self._min_cycle = min_cycle
+        self._max_cycle = max_cycle
+        self._name = name
+        self._state = None
+        self._brightness = None
+        self._gpio = pg.pi()
+        self._convfactor = (max_cycle - min_cycle) / 255
+        self._freq = frequency
+
+    @property
+    def brightness(self):
+        return self._gpio.get_PWM_frequency(self._pin)
+
+    @brightness.setter
+    def brightness(self, value):
+        self._gpio.hardware_PWM(self._pin, self._freq,
+                                (value * self._convfactor + self._min_cycle))
+
+    def turnOn(self):
+        self._gpio.hardware_PWM(self._pin, self._freq,
+                                (self._brightness * self._convfactor + self._min_cycle))
+
+    def turnOff(self):
+        self._gpio.hardware_PWM(self._pin, self._freq, self._min_cycle)
+
+    def getState(self):
+        if self._brightness > self._min_cycle:
+            return True
+        else:
+            return False
---
+++ pwmlight/tester.py
@@ -0,0 +1,5 @@
+import pwmlight
+
+light = pwmlight.pwmlight(18, 5000, 20000, 'test')
+
+light.brightness = 200
