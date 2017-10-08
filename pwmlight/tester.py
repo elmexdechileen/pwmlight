429a045da47e91e79989924c7dc23c56ea9c6d63
elmexdechileen <maxvanrooijen@gmail.com>
2017-10-08 14:07:07 +0200

    Fixed

--- pwmlight/tester.py
+++ pwmlight/tester.py
@@ -3,26 +3,38 @@ import time
 
 light = pwmlight.pwmlight(18, 50000, 1000000, 'test')
 
-#light.brightness = 15
+light.setBrightness(100)
 light.turnOn()
 
 time.sleep(1)
 
-light.brightness = 10
+light.setBrightness(20)
 
 time.sleep(1)
 
-
-
 light.turnOff()
 
+#while 1:
+#       for i in range(0, 50):
+#               light.setBrightness(i)
+#               time.sleep(.01)
+#
+#       for i in range(0, 50):
+#               light.setBrightness(50 - i)
+#               time.sleep(.01)
 
-#light.debug(20000)
+#light.debug(100000)
+
+#light.turnOff()
 
 
-print(str(light.brightness))
+#light.debug(100000)
 
-print(str(light.getState()))
+#light.turnOff()
+
+#print(str(light.brightness))
+
+#print(str(light.getState()))
 
 
 
